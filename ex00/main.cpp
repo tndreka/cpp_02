18aba2af34baacb8b1492afde1d7d49414981f98
tndreka <tndreka@student.42heilbronn.de>
2025-07-04 01:35:19 +0200

    ADD: compiling . . .

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,23 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:20:54 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/25 14:39:30 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/03 02:07:17 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main( void )
+int main()
 {
-	Fixed a;
-	Fixed b( a );
-	Fixed c;
-	c = b;
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	return 0;
+	int price = 100;
+	if (price == 100)
+	{
+		std::cout << "DOne";
+	}
+	else 
+		std::cout << "NOOOOOOOOOOOOPPPWWWWWWEE DOne";
+
 }
+
+// int main( void )
+// {
+// 	Fixed a;
+// 	Fixed b( a );
+// 	Fixed c;
+// 	c = b;
+// 	std::cout << a.getRawBits() << std::endl;
+// 	std::cout << b.getRawBits() << std::endl;
+// 	std::cout << c.getRawBits() << std::endl;
+// 	return 0;
+// }
