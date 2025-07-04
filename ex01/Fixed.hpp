8930291e613ad1c5b2e246e48c980f0793e70a29
tndreka <tndreka@student.42heilbronn.de>
2025-07-04 13:41:01 +0200

    ADD: created the Fixed class with all the atributtes and in Orthodoc Canonical Form. Declaret the private:values and also declared the Construtor: (default), copy, takes int as a param, takes foat as a param, copy assing operator and destructor. also initialzet the function that i will implement in .cpp file

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 13:20:29 by tndreka           #+#    #+#             */
+/*   Updated: 2025/07/04 13:36:43 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int fixed_point;
+		static const int fractional_bits = 8;
+	public:
+		Fixed();//Default constructor
+		Fixed(const Fixed& other);//COPY Constructor
+		Fixed(const int dec);//A constructor that takes a const INT as a parameter
+		Fixed(const float num);//A constructor that takes a const FLOAT as a parameter
+		Fixed& operator=(const Fixed& other);//Copy assign operator
+		~Fixed();//Destructor
+		int     getRawBits(void)const;
+        void    setRawBits(int const raw);
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+};
+
+#endif
