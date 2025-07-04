d32517fcdf0877efdf1a95b500649285c8f13e6d
tndreka <tndreka@student.42heilbronn.de>
2025-07-04 02:14:12 +0200

    ADD: Defined the constructors desructor implemented the functions

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 01:43:40 by tndreka           #+#    #+#             */
+/*   Updated: 2025/07/04 02:11:30 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixed_point(0)
+{
+    std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& other) : fixed_point(other.fixed_point)
+{
+    std::cout << "Copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called\n";
+    if (this != &other)
+        this->fixed_point = other.fixed_point;
+    return *this;
+}
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called\n";
+    return fixed_point;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called\n";
+    fixed_point = raw;
+}
