068774d4e4b7a1e677b6313dbb18af30a8b0ffe9
tndreka <tndreka@student.42heilbronn.de>
2025-07-04 02:15:12 +0200

    ADD: comparing my output with the one from the subject

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,32 +6,63 @@
 /*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:20:54 by tndreka           #+#    #+#             */
-/*   Updated: 2025/07/03 02:07:17 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/04 02:14:24 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Fixed.hpp"
 
-int main()
-{
-	int price = 100;
-	if (price == 100)
-	{
-		std::cout << "DOne";
-	}
-	else 
-		std::cout << "NOOOOOOOOOOOOPPPWWWWWWEE DOne";
-
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
 }
 
-// int main( void )
-// {
-// 	Fixed a;
-// 	Fixed b( a );
-// 	Fixed c;
-// 	c = b;
-// 	std::cout << a.getRawBits() << std::endl;
-// 	std::cout << b.getRawBits() << std::endl;
-// 	std::cout << c.getRawBits() << std::endl;
-// 	return 0;
-// }
+/*
+		Should output something similar to:
+$> ./a.out
+Default constructor called
+Copy constructor called
+Copy assignment operator called // <-- This line may be missing depending on your implementation
+getRawBits member function called
+Default constructor called
+Copy assignment operator called
+getRawBits member function called
+getRawBits member function called
+0
+getRawBits member function called
+0
+getRawBits member function called
+0
+Destructor called
+Destructor called
+Destructor called
+$>
+ */
+
+
+ //MY OUTPUT:
+ /*
+	
+baylozzi@baylozzi-ThinkPad-X395:~/Desktop/GITHUB_2024/cpp_02/ex00$ ./Fixed_point_Numbers 
+Default constructor called
+Copy constructor called
+Default constructor called
+Copy assignment operator called
+getRawBits member function called
+0
+getRawBits member function called
+0
+getRawBits member function called
+0
+Destructor called
+Destructor called
+Destructor called
+baylozzi@baylozzi-ThinkPad-X395:~/Desktop/GITHUB_2024/cpp_02/ex00$ ^C
+baylozzi@baylozzi-ThinkPad-X395:~/Desktop/GITHUB_2024/cpp_02/ex00$ 
+ */
